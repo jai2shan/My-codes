9081daa93baec26b2f01a770660d2b8a176c2f41
jai2shan <muralis2raj@gmail.com>
2021-03-03 13:38:42 +0530

    Create CoP-Solutions.py

---
+++ CoP-Solutions.py
@@ -0,0 +1,39 @@
+### Question 1:
+# Write a function to sum N natural numbers without using sum-function
+
+def sumNaturalNumbers(n):
+  sum = 0
+  for i in range(n+1):
+    sum = sum+i
+    
+  return sum
+
+sumNaturalNumbers(10)
+
+
+### Question 2:
+# Create a Cricle class and intialize it with radius. Make two methods getArea and getCircumference inside this class.
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def area(self):
+        return 3.1428*self.radius*self.radius
+    
+    def perimeter(self):
+        return 2*3.1428*self.radius
+    
+Circle(10).area()
+
+
+## Question 3:
+# Timer
+import time
+def timer(fn):
+    def inner(*args,**kwargs):
+        start = time.time()
+        result = fn(*args, **kwargs)
+        print('Time taken to execute {} is {} secs'.format(fn.__name__, (time.time()-start)))
+        return result
+    return inner
